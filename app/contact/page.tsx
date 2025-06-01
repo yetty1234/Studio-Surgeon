a86adaea0f3eba0f36fd59c7b5676855351a7c59
yetty1234 <yetsho003@outlook.com>
2025-06-01 12:57:26 -0400

    Update page.tsx

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -7,21 +7,21 @@ import { Card } from "../components/card";
 const socials = [
 	{
 		icon: <Twitter size={20} />,
-		href: "https://twitter.com/chronark_",
+		href: "https://twitter.com/yetsh0",
 		label: "Twitter",
-		handle: "@chronark_",
+		handle: "@YETSH0",
 	},
 	{
 		icon: <Mail size={20} />,
-		href: "mailto:dev@chronark.com",
+		href: "mailto:admin@yetsho.com",
 		label: "Email",
-		handle: "dev@chronark.com",
+		handle: "admin@yetsho.com",
 	},
 	{
 		icon: <Github size={20} />,
-		href: "https://github.com/chronark",
-		label: "Github",
-		handle: "chronark",
+		href: "https://instagram.com/mostdoubted",
+		label: "Instagram",
+		handle: "@mostdoubted",
 	},
 ];
 
