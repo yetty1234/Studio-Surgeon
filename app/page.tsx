186a6a3f0ed1ffceef39ab68347cdada152aa030
Yetsho <yeck00@Yecks-MacBook-Pro.local>
2025-06-01 13:52:15 -0400

    Your commit message here

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ const navigation = [
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black">
-      <nav className="my-8 animate-fade-in">
+      <nav className="my-16 animate-fade-in">
         <ul className="flex items-center justify-center gap-4">
           {navigation.map((item) => (
             <Link
@@ -33,7 +33,7 @@ export default function Home() {
       </h1>
 
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
-      <div className="my-8 text-center animate-fade-in">
+      <div className="my-16 text-center animate-fade-in">
         <h2 className="text-sm text-zinc-500 ">
           From {" "}
           <Link
