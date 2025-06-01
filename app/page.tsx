4252280294b208d42043d65e2daca920079d54fe
yetty1234 <yetsho003@outlook.com>
2025-06-01 13:08:16 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -26,7 +26,7 @@ export default function Home() {
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <Particles
         className="absolute inset-0 -z-10 animate-fade-in"
-        quantity={100}
+        quantity={300}
       />
       <h1 className="py-3.5 px-0.5 z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
         YETSHO
@@ -35,14 +35,14 @@ export default function Home() {
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <div className="my-16 text-center animate-fade-in">
         <h2 className="text-sm text-zinc-500 ">
-          I'm building{" "}
+          From {" "}
           <Link
             target="_blank"
             href="https://yetsho.com"
             className="underline duration-500 hover:text-zinc-300"
           >
-            unkey.dev
-          </Link> welcome to imaginary world turned reality.
+            yetsho.com
+          </Link> welcome to a imaginary world turned reality.
         </h2>
       </div>
     </div>
